25b364525cc77ae8ad0f16cc3d12f8aa00a78ab1
Adam Day <dayadamuga@gmail.com>
2019-11-12 15:40:44 -0500

    seatgeek response working

--- keys.js
+++ keys.js
@@ -4,3 +4,8 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.seatgeek = {
+  id: process.env.SEATGEEK_ID,
+  secret: process.env.SEATGEEK_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,4 +1,29 @@
-//require("dotenv").config();
-
+const axios = require("axios");
+require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+const searchTerm = process.argv.slice(3).join("-");
+//var spotify = new Spotify(keys.spotify);
+var seatgeek = new SeatGeek(keys.seatgeek);
+
+function SeatGeek(keys) {
+    this.URL = `https://api.seatgeek.com/2/events?client_id=${keys.id}&performers.slug=${searchTerm}`;
+};
+
+if (process.argv[2] === "concert-this") {
+    axios.get(seatgeek.URL).then(function(response) {
+        console.log(response.data);
+    });
+
+};
+
+if (process.argv[2] === "spotify-this-song") {
+    
+};
+
+if (process.argv[2] === "movie-this") {
+    
+};
+
+if (process.argv[2] === "do-what-it-says") {
+    
+};
