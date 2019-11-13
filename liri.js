81548e3102bbe44bc4b6133012939a4f4807786e
Adam Day <dayadamuga@gmail.com>
2019-11-12 22:16:40 -0500

    finish do what it says

--- liri.js
+++ liri.js
@@ -2,27 +2,31 @@ const axios = require("axios");
 require("dotenv").config();
 var keys = require("./keys.js");
 const moment = require("moment");
+const fs = require("fs");
 var Spotify = require('node-spotify-api');
 let searchTermHyphen = process.argv.slice(3).join("-");
 let searchTermSpace = process.argv.slice(3).join(" ");
 var spotify = new Spotify(keys.spotify);
-var seatgeek = new SeatGeek(keys.seatgeek);
-
-function SeatGeek(keys) {
-    this.URL = `https://api.seatgeek.com/2/events?client_id=${keys.id}&performers.slug=${searchTermHyphen}`;
-};
+var seatgeek = keys.seatgeek.id;
 
 if (process.argv[2] === "concert-this") {
-    axios.get(seatgeek.URL).then(function (response) {
+    concertThis(searchTermHyphen);
+};
+function concertThis(searchTermHyphen) {
+    const URL = `https://api.seatgeek.com/2/events?client_id=${seatgeek}&performers.slug=${searchTermHyphen}`
+    axios.get(URL).then(function (response) {
         const answer = response.data.events[0];
         console.log(answer.venue.name);
         console.log(answer.venue.extended_address);
         //formatting date with moment
-        console.log(moment(answer.datetime_utc).format("MM/DD/YYYY"));
+        console.log(moment(answer.datetime_local).format("MM/DD/YYYY"));
     });
 };
 
 if (process.argv[2] === "spotify-this-song") {
+    spotifyThisSong(searchTermSpace);
+};
+function spotifyThisSong(searchTermSpace) {
 
     // setting search parameter to the sign by ace of base if undeclared
     if (!searchTermSpace) {
@@ -62,10 +66,10 @@ if (process.argv[2] === "spotify-this-song") {
 };
 
 
-
-
-
 if (process.argv[2] === "movie-this") {
+    movieThis(searchTermHyphen);
+};
+function movieThis(searchTermHyphen) {
 
     // setting search parameter to My Nobody if undeclared
     if (!searchTermHyphen) {
@@ -77,15 +81,27 @@ if (process.argv[2] === "movie-this") {
         const answer = response.data;
         console.log(answer.Title);
         console.log(answer.Year);
-        console.log("IMDB rating: " +answer.Ratings[0].Value);
+        console.log("IMDB rating: " + answer.Ratings[0].Value);
+        console.log("Rotten Tomatoes rating: " + answer.Ratings[1].Value);
         console.log(answer.Country);
         console.log(answer.Language);
         console.log(answer.Plot);
         console.log(answer.Actors);
     });
-
 };
 
 if (process.argv[2] === "do-what-it-says") {
-
+    fs.readFile('random.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        const dataArray = data.split(",")
+        if (dataArray[0] === "concert-this") {
+            concertThis(dataArray[1].replace(" ", "-"));
+        };
+        if (dataArray[0] === "spotify-this-song") {
+            spotifyThisSong(dataArray[1]);
+        };
+        if (dataArray[0] === "movie-this") {
+            movieThis(dataArray[1].replace(" ", "-"));
+        };
+    });
 };
